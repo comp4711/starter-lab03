23613f3d13e7c699be823698450a048b4c6dffe2
Pouya Sarang <psarang@my.bcit.ca>
2015-09-24 15:14:44 -0700

    last, sleep, welcome, and gimme controllers routing Signed-off-by:Pouya Sarang <psarang@my.bcit.ca>

--- application/config/routes.php
+++ application/config/routes.php
@@ -40,6 +40,12 @@
 
 $route['default_controller'] = "welcome";
 $route['404_override'] = '';
+$route['last'] = 'Last/last.php';
+$route['sleep'] = 'first/zzz';
+$route['lock/(:any)/(:any)'] = 'welcome/shucks';
+$route['show/(:num)'] = 'first/gimme/$1';
+
+
 
 
 /* End of file routes.php */
