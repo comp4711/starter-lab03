2dd5136ac068d502c9384a2ac1afdd567ec1c49f
Nadia Dobrianskaia <nadezhdadobrianskaia@gmail.com>
2015-09-24 16:02:38 -0700

    fixing part 3 of the lab

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -27,4 +27,16 @@ class First extends Application{
 
         $this->render();
     }
+    
+        function zzz() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->get(1);
+        
+        $this->data['who'] = $source['who'];
+        $this->data['mug'] = $source['mug'];
+        $this->data['what'] = $source['what'];
+
+        $this->render();
+    }
 }
