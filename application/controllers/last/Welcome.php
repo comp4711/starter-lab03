f6b6b6496240711554d5233d1f0529f5549c3114
Nadia Dobrianskaia <nadezhdadobrianskaia@gmail.com>
2015-09-24 15:34:20 -0700

    fixing the second link under last instructions

---
+++ application/controllers/last/Welcome.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Welcome
+ *
+ * @author Nina
+ */
+class Welcome extends Application{
+    function __construct() {
+        parent::__construct();
+    }
+    
+        function index() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->last();
+        
+        $this->data['who'] = $source['who'];
+        $this->data['mug'] = $source['mug'];
+        $this->data['what'] = $source['what'];
+
+        $this->render();
+    }
+}
