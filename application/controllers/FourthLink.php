b6d0ddde9a7ef904f67eea3b5b4f6f0752ebe0a3
Nadia Dobrianskaia <nadezhdadobrianskaia@gmail.com>
2015-09-24 17:07:36 -0700

    link 6 fixed.  ie the dunno

---
+++ application/controllers/FourthLink.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of FourthLink
+ *
+ * @author Nina
+ */
+class FourthLink extends Application{
+     function __construct() {
+        parent::__construct();
+    }
+    
+        function index() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->get(4);
+        
+        $this->data['who'] = $source['who'];
+        $this->data['mug'] = $source['mug'];
+        $this->data['what'] = $source['what'];
+
+        $this->render();
+    }
+
+}
