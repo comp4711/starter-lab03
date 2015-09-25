07f357aa1eb9d0e15ad4abe5e43edf72d7570eb1
jason <jason@jason-S400CA>
2015-09-24 17:13:48 -0700

    Fixed the second link

---
+++ application/controllers/last/Welcome.php
@@ -0,0 +1,24 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class Welcome extends Application {
+    function __construct() {
+        parent::__construct();
+    }
+
+    //-------------------------------------------------------------
+    //  The normal pages
+    //-------------------------------------------------------------
+
+    function index() {
+        $this->data['pagebody'] = 'justone';
+        $record = $this->quotes->last();
+        $this->data = array_merge($this->data, $record);
+        $this->render();
+    }
+
+}
