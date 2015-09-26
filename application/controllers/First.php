0394d6589098f235695e7b237f5be42052f769ae
TheLawrence <lawsarmiento@gmail.com>
2015-09-26 14:32:36 -0700

    commit for first fix to develop branch Signed-off-by:TheLawrence <lawsarmiento@gmail.com>

---
+++ application/controllers/First.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of First
+ *
+ * @author Lawrence
+ */
+class First extends Application {
+    
+    function index() {
+        $this->data['pagebody'] = 'justone';
+        
+        $source = $this->quotes->first();
+        
+        $this->data = array_merge($this->data, $source);
+        
+        $this->render();
+    }
+    
+}
