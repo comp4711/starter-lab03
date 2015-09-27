093fe9ede9fbdeff409b9dd1b9196f4c09d30d4c
adamh <adamh@DESKTOP-HU42152>
2015-09-27 10:10:48 -0700

    last

---
+++ application/controllers/last/Welcome.php
@@ -0,0 +1,21 @@
+<?php
+/* The first link on the nav bar */
+class Welcome extends Application {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    //-------------------------------------------------------------
+    //  The normal pages
+    //-------------------------------------------------------------
+
+    function index() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        // get the last quote
+        $record = $this->quotes->last();
+        $this->data = array_merge($this->data, $record);
+        $this->render();
+    }
+
+}
