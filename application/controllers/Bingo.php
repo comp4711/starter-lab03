c39182ede384551898856f46128e266ba1d566b4
adamh <adamh@DESKTOP-HU42152>
2015-09-27 12:40:47 -0700

    wise/bingo

---
+++ application/controllers/Bingo.php
@@ -0,0 +1,21 @@
+<?php
+/* The Bingo controller */
+class Bingo extends Application {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    //-------------------------------------------------------------
+    //  The normal pages
+    //-------------------------------------------------------------
+
+    function index() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        // gets the 5th quote
+        $record = $this->quotes->get(5);
+        $this->data = array_merge($this->data, $record);
+        $this->render();
+    }
+
+}
