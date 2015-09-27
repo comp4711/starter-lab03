f88d0a630f24f1a01759ddba92d8ada7f4f0ce97
adamh <adamh@DESKTOP-HU42152>
2015-09-27 12:49:12 -0700

    comp4711/wisdom

--- application/controllers/Bingo.php
+++ application/controllers/Bingo.php
@@ -17,5 +17,13 @@ class Bingo extends Application {
         $this->data = array_merge($this->data, $record);
         $this->render();
     }
+    
+    function wisdom() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        // gets the 6th quote
+        $record = $this->quotes->get(6);
+        $this->data = array_merge($this->data, $record);
+        $this->render();
+    }
 
 }
