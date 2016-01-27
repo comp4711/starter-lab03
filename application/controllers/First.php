dbdd44742bee706275e3684835bc70cca05fcb78
Jox <joxched@gmail.com>
2016-01-27 11:06:06 -0800

    fix first link

---
+++ application/controllers/First.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Our homepage. Show a table of all the author pictures. Clicking on one should show their quote.
+ * Our quotes model has been autoloaded, because we use it everywhere.
+ *
+ * controllers/First.php
+ *
+ * ------------------------------------------------------------------------
+ */
+class First extends Application {
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    //-------------------------------------------------------------
+    //  The normal pages
+    //-------------------------------------------------------------
+
+    function index()
+    {
+        //fix first route
+        $this->load->model('quotes');
+        $record = $this->quotes->first();
+        $this->data['pagebody'] = 'justone';
+
+        $this->data = array_merge($this->data, $record);
+
+        $this->render();
+
+    }
+
+}
+
+/* End of file Welcome.php */
+/* Location: application/controllers/Welcome.php */
