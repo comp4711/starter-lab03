c01daf63e5c10d4b6178e78291594571192457f1
Anthony Smith <anthony@smith.lt>
2016-01-28 23:30:39 -0800

    Change the method used to retreive quote data

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -10,7 +10,7 @@ class First extends Application {
     $this->data['pagebody'] = 'justone'; // this is the view we want shown
 
     // get the data from the first quote to be shown
-    $source = $this->quotes->get(1);
+    $source = $this->quotes->first();
     $this->data['mug'] = $source['mug'];
     $this->data['who'] = $source['who'];
     $this->data['what'] = $source['what'];
