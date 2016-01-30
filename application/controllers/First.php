c61f01de0313354bf0887c80d7c250fcddb08fb9
Matthew Wong <matthewwong3.14@gmail.com>
2016-01-30 00:14:21 -0800

    fixed error with which record

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -28,7 +28,7 @@ class First extends Application {
         function zzz()
         {
             $this->data['pagebody'] = 'justone';
-            $record = $this->quotes->get('2');
+            $record = $this->quotes->get('1');
             $this->data = array_merge($this->data, $record);
             $this->render();
         }
