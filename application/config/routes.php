e0dc214a6e090440d5d3f227fdc2f3d628f88597
Matthew Wong <matthewwong3.14@gmail.com>
2016-01-30 01:17:13 -0800

    Bingo feature added

--- application/config/routes.php
+++ application/config/routes.php
@@ -44,6 +44,7 @@ $route['last'] = 'last/Welcome';
 $route['first'] = 'first';
 $route['(:any)'] = 'First/zzz';
 $route['show/(:num)'] = 'First/gimmie/3'; 
+$route['([a-zA-Z]{4})/bingo'] = "bingo/index";
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
