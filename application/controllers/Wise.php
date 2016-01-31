f04a0793f445a501b3129a1abf5f619f53b08356
josephwithay <yoseph_jo@hotmail.com>
2016-01-31 04:27:25 -0800

    Feature 8 is done. Signed-off-by:josephwithay <yoseph_jo@hotmail.com>

--- application/controllers/Wise.php
+++ application/controllers/Wise.php
@@ -12,9 +12,13 @@ class Wise extends Application
 	}
 	function bingo() 
 	{
-		$this->data['pagebody'] = 'justone'; // this is the view we want shown
+		$this->data['pagebody'] = 'justone';	// this is the view we want shown
+		// get first author data, to pass on to our view
 		$record = $this->quotes->get(5);
+		
+		// Put into respective data view
 		$this->data = array_merge($this->data, $record);
+		
 		$this->render();
 	}
 }
