9b472e6418f5c9fa66d737d43740920fc6e8ad99
Khang <Khang@school>
2016-01-30 23:13:05 -0800

    Merge upstream/Develop into Khang's_2nd-5th-&7th_Link
    Conflicts:
    	application/config/routes.php
    	application/controllers/First.php
    Signed-off-by:Khang <Khang@school>

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -27,50 +27,18 @@ class First extends Application
         $this->data = array_merge($this->data, $record);
         $this->render();
     }
-}
-<?php
-
-/**
- * Our homepage. Show a table of all the author pictures. Clicking on one should show their quote.
- * Our quotes model has been autoloaded, because we use it everywhere.
- * 
- * controllers/First.php
- * controller for the 5th Link: Show
- *
- * ------------------------------------------------------------------------
- */
-class First extends Application {
-
-	function __construct()
-	{
-		parent::__construct();
-	}
-
-	//-------------------------------------------------------------
-	//  The normal pages
-	//-------------------------------------------------------------
-
-	function index()
-	{
-		$this->data['pagebody'] = 'justone';	// this is the view we want shown
-		
-                $record = $this->quotes->get();
-                $this->data = array_merge($this->data, $record); 
-
-		$this->render();
-	}
         
-        function gimme($id){
-                $this->data['pagebody'] = 'justone';	
-		
-                $record = $this->quotes->get($id); 
-                $this->data = array_merge($this->data, $record); 
+    function gimme($id){
+        $this->data['pagebody'] = 'justone';	
 
-		$this->render();
-                      
-        }
+        $record = $this->quotes->get($id); 
+        $this->data = array_merge($this->data, $record); 
+
+        $this->render();             
+    }
 
 }
 
-/* End of file First.php */
-/* Location: application/controllers/First.php */
+    /* End of file First.php */
+    /* Location: application/controllers/First.php */
+?>
